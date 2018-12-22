ae547bf79cac242ab485cd21aa870748f8c2c067
Elizabeth Callam <ecallam@covermymeds.com>
2018-12-22 14:17:35 -0500

    Style and gem updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_many :comments
-  validates :title, :content, :category_id, presence: true, length: {minimum: 5}
+  validates :title, :content, :category_id, presence: true
+  validates :title, length: {minimum: 5}
 end
