c18134a4b98cdb85c45fcafbecc1bee2006f5c9e
Elizabeth Callam <ecallam@covermymeds.com>
2019-06-13 08:53:20 -0400

    Fun with JavaScript styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+
+function co(lor) {
+  return (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  "#" + lor : co(lor)};
+$(function() {
+  document.getElementById("name").style.color = co("a");
+});
