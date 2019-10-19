7e0f6fd149321e0de3e4d56f2405633c39ff1b7d
Elizabeth Callam <bethcallam2@gmail.com>
2019-10-19 15:10:35 -0400

    Update styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,5 @@
 //= require_tree .
 
 function co(lor) {
-  return (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  "#" + lor : co(lor)};
-$(function() {
-  document.getElementById("name").style.color = co("a");
-});
+  return document.getElementById(event.target.id).style.color = (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  "#" + lor : co(lor)
+};
